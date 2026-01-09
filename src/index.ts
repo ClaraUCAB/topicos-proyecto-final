3d033f0d56c66f919ca00fc0570683ddd8b4fd26
ClaraUCAB <146204299+ClaraUCAB@users.noreply.github.com>
2026-01-09 08:13:59 -0400

    Added todo

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ app.use(express.json());
 
 app.use("/images", imageRoutes);
 
+// TODO: Hacer el puerto una variable de entorno
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
 });
+
